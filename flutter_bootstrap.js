ee767060509f946342ca70e3d368bc668a0ca47b
degtyarikup-ui <degtyarik.up@gmail.com>
2026-08-29 17:00:17 +0300

    fix: deploy verified Gemini 3.6 Flash scanner

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3801228454" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2041857910" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
