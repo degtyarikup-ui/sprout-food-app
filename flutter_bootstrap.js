07f635e89ef6965aae7ca6722e714a3e57e4b1ef
degtyarikup-ui <degtyarik.up@gmail.com>
2026-08-30 15:18:46 +0300

    feat: deploy Telegram Mini App Web version with Family Sharing and Avatar fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1786319804" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "927304763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
